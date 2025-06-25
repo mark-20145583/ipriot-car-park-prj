21a3c3782f3d41752c580b9bf2635c5103766c81
20145583 - Mark <20145583@tafe.wa.edu.au>
2025-06-25 18:54:20 +0800

    implement display updates

--- src/car_park.py
+++ src/car_park.py
@@ -22,11 +22,25 @@ class CarPark:
             elif isinstance(component,Display):
                 self.displays.append(component)
 
-    def add_car(self):
-        pass
+    def add_car(self, plate):
+        self.plates.append(plate)
+        self.update_displays()
 
-    def remove_car(self):
-        pass
+    def remove_car(self, plate):
+        self.plates.remove(plate)
+        self.update_displays()
 
     def update_displays(self):
-        pass
+        data = {
+            "available_bays": self.available_bays,
+            "temperature": 25
+        }
+        for display in self.displays: display.update(data)
+
+    @property
+    def available_bays(self):
+        if len(self.plates) >= self.capacity:
+            return 0
+        else:
+            return self.capacity - len(self.plates)
+
--- src/display.py
+++ src/display.py
@@ -7,3 +7,7 @@ class Display:
 
     def __str__(self):
         return(f"Display {self.id}: {self.message}")
+
+    def update(self, data):
+        for key, value in data.items():
+            print(f"{key}: {value}")
