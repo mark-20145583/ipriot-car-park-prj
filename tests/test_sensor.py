099fff009842725bbe66b6e2e744810fbffaa1c4
mark-20145583 <20145583@tafe.wa.edu.au>
2025-06-27 08:09:41 +0800

    sensor test corrected

--- tests/test_sensor.py
+++ tests/test_sensor.py
@@ -5,19 +5,15 @@ from sensor import Sensor
 
 class TestSensor(Sensor):
 
+    def setUp(self):
+         self.car_park = CarPark("123 Example Street", 100)
+
     def update_car_park(self, plate):
         return True
 
-
-
-def test_sensor():
-
-    try:
-        car_park = CarPark("123 Example Street", 100)
-        sensor = TestSensor(1,True,car_park)
-        print("Test successful.")
-    except:
-        print("An error occurred")
+    def test_sensor(self):
+        with self.assertRaises(ValueError)
+            self.sensor = TestSensor(1,True,self.car_park)
 
 
 
